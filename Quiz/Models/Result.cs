bb9f0e7b59feb2fa462316d6932446f37c833698
Andrea De cunha <andydecunha117@gmail.com>
2026-04-05 14:45:11 +0100

    feat: add Quiz API models and SQLite setup

---
+++ Quiz/Models/Result.cs
@@ -0,0 +1,13 @@
+﻿namespace Quiz.Models
+{
+    public class Result
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public int QuizItemId { get; set; }          // ← updated
+        public QuizItem? QuizItem { get; set; }      // ← updated
+        public int Score { get; set; }
+        public DateTime TakenAt { get; set; } = DateTime.UtcNow;
+    }
+}
